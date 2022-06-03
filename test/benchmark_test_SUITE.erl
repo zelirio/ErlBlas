f604bae9a6a6b13c6a9f3992e9ada0fb2944dc53
zelirio <49032158+zelirio@users.noreply.github.com>
2022-06-03 10:29:22 +0200

    benchmark test with mean

--- test/benchmark_test_SUITE.erl
+++ test/benchmark_test_SUITE.erl
@@ -9,12 +9,15 @@ benchmark_testi() ->
         erlBlas:get_max_length()).
 
 max_test() ->
-    %ok.
-    run(63).
+    {timeout, 1000, run(20, 40, [])}.
 
-run(67) ->
+run(80, 0, Acc) ->
     ok;
-run(N) ->
+run(N, 0, Acc) ->
+    erlang:display(N),
+    erlang:display(stats(Acc)),
+    run(N + 1, 40, []);
+run(N, T, Acc) ->
     M = utils:generateRandMat(N, N),
     Mat = numerl:matrix(M),
     M2 = utils:generateRandMat(N, N),
@@ -23,8 +26,8 @@ run(N) ->
     {Time, _} = timer:tc(numerl, dgemm, [1, 1, 1.5, Mat, Mat2, 2.0, C]),
     %erlang:display(
     %numerl:mtfl(C)),
-    erlang:display({time, N, Time}),
-    run(N + 1).
+    %erlang:display({time, N, Time}),
+    run(N, T - 1, [Time | Acc]).
 
 run2(_, 0) ->
     ok;
@@ -35,3 +38,29 @@ run2(N, I) ->
     {Time, _} = timer:tc(numerl, daxpy, [1.0, Mat, Mat]),
     erlang:display({time, N, Time}),
     run2(N, I - 1).
+
+stats(List) ->
+    case List of
+        [X] ->
+            {X, 0};
+        _ ->
+            {mean(List), var(List, mean(List))}
+    end.
+
+mean(List) ->
+    %erlang:display(List),
+    lineSum(List) / length(List).
+
+var(List, Mean) ->
+    lineSum(lists:map(fun(Elem) -> (Elem - Mean) * (Elem - Mean) end, List)) / length(List).
+
+lineSum([H | T]) ->
+    lineSum(T, H).
+
+lineSum(List, Acc) ->
+    case List of
+        [H | T] ->
+            lineSum(T, Acc + H);
+        [] ->
+            Acc
+    end.
